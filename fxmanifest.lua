913082873c3fa8ae3d7e1695bbb00c24ab4d9f40
Luna <hnkohl@gmail.com>
2023-04-13 12:37:01 -0500

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'lf-timecycle'
+version '1.0'
+description 'FiveM resource for quickly viewing timecycle modifiers.'
+author 'Luna Fox'
+contact 'Sharkie#6969'
+
+client_scripts {
+    '@menuv/menuv.lua',
+    'timecycle.lua',
+    'list.lua'
+}
+
+dependencies {
+    'menuv'
+}
---
+++ timecycle.lua
@@ -0,0 +1,32 @@
+--- MenuV Menu
+---@type Menu
+local menu = MenuV:CreateMenu(false, 'Timecycle Modifiers', 'topright', 255, 0, 0, 'size-125', 'example', 'menuv', 'timecycle_home')
+local menu2 = MenuV:CreateMenu(false, 'Timecycle Modifiers', 'topright', 255, 0, 0, 'size-125', 'example', 'menuv', 'timecycle_list')
+
+local menu_button = menu:AddButton({ icon = '🔆', label = 'View Timecycles', value = menu2, description = 'Opens a list of available timecycles to test.' })
+local default_button = menu:AddButton({ icon = '↩️', label = 'Reset to Default', value = default_button, description = 'Resets timecycle to default.' })
+local slider = menu2:AddSlider({
+    icon = '🔆',
+    label = 'Timecycle',
+    value = 'demo',
+    values = {}
+})
+
+menu:On('select', function()
+    for _, v in pairs(timecycles) do
+        slider.AddValue(slider, { label = v, value = v, description = v })
+    end
+end)
+
+--- Events
+slider:On('change', function(item, value)
+    SetTimecycleModifier(timecycles[value])
+end)
+
+default_button:On('select', function(item)
+    SetTimecycleModifier('default')
+end)
+
+RegisterCommand("timecycle", function()
+    MenuV:OpenMenu(menu)
+end, "admin")
